958c1183cabdfdd77e7675bd034d8aa1a6e0c51d
Diogoribeirott <121639984+Diogoribeirott@users.noreply.github.com>
2023-01-17 18:00:05 -0300

    Problema arrumado

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ btnSubmit.addEventListener("click", (ev) => {
 function addtask() {
   const inputtop = document.querySelector(".inputtop");
 
-  if (inputtop) {
+  if (inputtop.value) {
     const ul = document.getElementById("ul");
     const li = document.createElement("li");
     const span = document.createElement("span");
@@ -19,9 +19,11 @@ function addtask() {
     div.classList = "buttonopcao";
     const button = document.createElement("button");
     button.innerHTML = '<ion-icon name="checkmark-outline"></ion-icon>';
-    button.setAttribute("onclick", "feito()");
+    button.setAttribute("onclick", "feito(this)");
+
     const button2 = document.createElement("button");
     button2.innerHTML = '<ion-icon name="close-outline"></ion-icon>';
+    button2.setAttribute("onclick", "closee(this)");
 
     div.append(button, button2);
     li.append(span, div);
@@ -33,4 +35,15 @@ function addtask() {
   inputtop.value = "";
 }
 
-function feito() {}
+function feito(botaoCerto) {
+  div = botaoCerto.parentNode;
+  task = div.parentNode;
+  task.classList.toggle("done");
+  //o toggle faz com que ele verifique se tem a class, se tiver ele tira, se tiver ela tira
+}
+
+function closee(botaoclose) {
+  div = botaoclose.parentNode;
+  task = div.parentNode;
+  task.remove();
+}
